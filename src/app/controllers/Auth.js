129b3ba167b9fd88b0dd25290f444823055e94ab
Guilherme Fabricio <guilherme.fabricio.115@gmail.com>
2023-06-25 13:16:25 -0300

    Login feito com criptografia jwt

--- src/app/controllers/Auth.js
+++ src/app/controllers/Auth.js
@@ -3,8 +3,16 @@ import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
 import User from "@/app/schemas/User";
+import authConfig from "@/config/Auth"
 
 const router = new Router();
+
+const generateToken = params => {
+    return jwt.sign(
+        params, authConfig.secret, {
+            expiresIn: 86400,
+        });
+};
 
 router.post('/register', (req, res) => {
 
@@ -33,7 +41,34 @@ router.post('/register', (req, res) => {
 });
 
 router.post("/login", (req, res) => {
+    const { email, password } = req.body;
 
+    User.findOne({ email })
+        .select("+password")
+        .then(user => {
+            if (user) {
+                bcrypt
+                    .compare(password, user.password)
+                    .then(result => {
+                        if (result) {
+                            const token = generateToken({ uid: user.id })
+                            return res.send({ token: token, tokenExpiration: "1d" })
+                        } else {
+                            return res.status(400).send({ error: "Senha invalida" });
+                        }
+                    })
+                    .catch(error => {
+                        console.error("Error ao verificar senha", error);
+                        return res.status(500).send({ error: "user not found" });
+                    });
+            } else {
+                return res.status(404).send({ error: "user not found" });
+            }
+
+        }).catch(error => {
+            console.error("Erro ao logar", error);
+            return res.status(500).send({ error: "internal error server" });
+        })
 });
 
 router.post("/forgot-password", (req, res) => {
